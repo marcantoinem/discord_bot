1c747d020c24a2b80423196b4469310237730e6a
marcantoinem <marcantoine_manningham@outlook.com>
2022-12-13 20:12:33 -0500

    First implementation of slash command

--- src/commands/join.rs
+++ src/commands/join.rs
@@ -1,56 +1,29 @@
-use serenity::{builder::*, collector::ModalInteractionCollector, model::prelude::*, prelude::*};
+use serenity::{
+    builder::*, collector::ComponentInteractionCollector, model::prelude::*, prelude::*,
+};
 
 pub async fn run(ctx: &Context, interaction: &CommandInteraction) -> Result<(), serenity::Error> {
-    // let modal = CreateQuickModal::new("About you")
-    //     .timeout(std::time::Duration::from_secs(600))
-    //     .short_field("First name")
-    //     .short_field("Last name")
-    //     .paragraph_field("Hobbies and interests");
-
-    // let response = interaction.quick_modal(ctx, modal).await?.unwrap();
-
-    // let inputs = response.inputs;
-    // let (first_name, last_name, hobbies) = (&inputs[0], &inputs[1], &inputs[2]);
-
-    // response
-    //     .interaction
-    //     .create_response(
-    //         ctx,
-    //         CreateInteractionResponse::Message(CreateInteractionResponseMessage::new().content(
-    //             format!("**Name**: {first_name} {last_name}\n\nHobbies and interests: {hobbies}"),
-    //         )),
-    //     )
-    //     .await?;
-    // Ok(())
-    // println!("I'm here.");
     let select_menu = CreateSelectMenu::new("hello", CreateSelectMenuKind::User);
-    let components = vec![CreateActionRow::SelectMenu(select_menu)];
-    let modal =
-        CreateModal::new(interaction.id.to_string(), "Team creation").components(components);
-    CreateInteractionResponse::Modal(modal)
+    let message = CreateInteractionResponseMessage::new()
+        .select_menu(select_menu)
+        .content("Je suis un retour.")
+        .ephemeral(true);
+    CreateInteractionResponse::Message(message)
         .execute(ctx, interaction.id, &interaction.token)
         .await?;
-    let modal_interaction = ModalInteractionCollector::new(&ctx.shard)
-        .custom_ids(vec!["first_button".to_string()])
+    if let Some(interaction) = ComponentInteractionCollector::new(&ctx.shard)
         .collect_single()
-        .await;
+        .await
+    {
+        println!("{:?}", interaction.message.id);
+        let response = CreateInteractionResponseMessage::new()
+            .content("Thank's for your response.")
+            .components(vec![]);
+        CreateInteractionResponse::UpdateMessage(response)
+            .execute(ctx, interaction.id, &interaction.token)
+            .await?;
+    }
 
-    let modal_interaction = match modal_interaction {
-        Some(x) => x,
-        None => return Err(serenity::Error::Other("No response from the modal.")),
-    };
-    let mut inputs = modal_interaction.data.components.iter();
-
-    let input = inputs.next().unwrap();
-
-    modal_interaction
-        .create_response(
-            ctx,
-            CreateInteractionResponse::Message(
-                CreateInteractionResponseMessage::new().content(format!("{:?}", input)),
-            ),
-        )
-        .await?;
     Ok(())
 }
 
